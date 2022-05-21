cf5657b008ef3d619f6de9f655cd290bdd166010
Renukavedulla1201 <97425851+Renukavedulla1201@users.noreply.github.com>
2022-05-21 14:37:12 +0530

    #include<stdio.h> int main() {     int a,b,i,t;     scanf("%d%d",&a,&b);     if(a<b)     {         t=b;         b=a;         a=t;     }     for(i=b;i>=1;i--)     {         if(a%i==0&&b%i==0)         {             printf("%d",i);             break;         }     } }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int a,b;
+    int a,b,i,t;
     scanf("%d%d",&a,&b);
-    while(a!=b)
+    if(a<b)
     {
-        if(a>b)
+        t=b;
+        b=a;
+        a=t;
+    }
+    for(i=b;i>=1;i--)
+    {
+        if(a%i==0&&b%i==0)
         {
-            a=a-b;
-        }
-        else
-        {
-            b=b-a;
+            printf("%d",i);
+            break;
         }
     }
-    printf("%d
-",a);
-    return 0;
 }
