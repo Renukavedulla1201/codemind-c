d2e8beec29bd92f6a62860f053b407471b3d7607
Renukavedulla1201 <97425851+Renukavedulla1201@users.noreply.github.com>
2022-06-30 14:30:45 +0530

    #include<stdio.h> main() {     int a,b,i=1;     scanf("%d%d",&a,&b);     while(a*i%b!=0)     {         i++;     }     printf("%d",a*i); }

--- LCM.c
+++ LCM.c
@@ -1,16 +1,11 @@
 #include<stdio.h>
-int main()
+main()
 {
-    int i,n1,n2,gcd,lcm;
-    scanf("%d %d",&n1,&n2);
-    for(i=1;i<=n1&&i<=n2;i++)
+    int a,b,i=1;
+    scanf("%d%d",&a,&b);
+    while(a*i%b!=0)
     {
-        if(n1%i==0&&n2%i==0)
-        {
-            gcd=i;
-        }
+        i++;
     }
-    lcm=(n1*n2)/gcd;
-    printf("%d",lcm);
-    return 0;
+    printf("%d",a*i);
 }
