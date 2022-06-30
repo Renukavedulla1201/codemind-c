6b59615e512bf436d5f0c33d417a886607879279
Renukavedulla1201 <97425851+Renukavedulla1201@users.noreply.github.com>
2022-06-30 14:29:39 +0530

    #include<stdio.h> int main() {     int n,c=0,x=0,y=0,i,d;     scanf("%d",&n);     for(i=2;i<n;i++)     {         if(n%i==0)         {             c++;         }     }     if(c==0)     {         while(n>0)         {             d=n%10;             for(i=2;i<d;i++)             {                 if(d%i==0)                 {                     x++;                 }             }             if(d==1)             {                 y++;             }             n=n/10;         }     }     if(c==0&&x==0&&y==0)     {         printf("Mega Prime");     }     else     {         printf("Not Mega Prime");     }     return 0; }

--- mega_prime.c
+++ mega_prime.c
@@ -1,38 +1,35 @@
 #include<stdio.h>
-bool isprime(int n)
+int main()
 {
-    for(int i=2;i<n/2;i++)
+    int n,c=0,x=0,y=0,i,d;
+    scanf("%d",&n);
+    for(i=2;i<n;i++)
     {
         if(n%i==0)
         {
-            return false;
+            c++;
         }
     }
-    return true;
-}
-bool isdigitprime(int n)
-{
-    int temp=n,digit;
-    while(temp)
+    if(c==0)
     {
-        digit=temp%10;
-        if(digit!=2 && digit!=3 && digit!=5 && digit!=7)
+        while(n>0)
         {
-            return false;
+            d=n%10;
+            for(i=2;i<d;i++)
+            {
+                if(d%i==0)
+                {
+                    x++;
+                }
+            }
+            if(d==1)
+            {
+                y++;
+            }
+            n=n/10;
         }
-        temp=temp/10;
     }
-    return true;
-}
-bool isfullprime(int n)
-{
-    return(isdigitprime(n) && isprime(n));
-}
-int main()
-{
-    int n;
-    scanf("%d",&n);
-    if(isfullprime(n))
+    if(c==0&&x==0&&y==0)
     {
         printf("Mega Prime");
     }
@@ -40,4 +37,5 @@ int main()
     {
         printf("Not Mega Prime");
     }
+    return 0;
 }
